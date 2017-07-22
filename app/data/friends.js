2ae91998ee64b56d02f84810cf99c95b9d030b57
alanttran <mr.alan.tran@gmail.com>
2017-07-22 02:12:37 -0700

    finish project

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friends = [{
+	name: "Alan",
+	photo: "https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/10931005_10152754139819495_1475806810212098281_n.jpg?oh=cc314ceb1be310c09ef3911134f8cfa9&oe=5A06FCBE",
+	scores: [3,2,5,2,4,1,3,4,2,1]
+},
+{
+	name: "Justin",
+	photo: "http://static.wixstatic.com/media/2d23a2_83f78663a28a4152a5125a3dcf43e726.jpg/v1/fill/w_619,h_845/2d23a2_83f78663a28a4152a5125a3dcf43e726.jpg",
+	scores: [3,2,1,3,4,4,2,1,4,2]
+},
+{
+	name: "Danny",
+	photo: "https://pbs.twimg.com/profile_images/681369932207013888/CHESpTzF.jpg",
+	scores: [1,1,1,1,1,1,1,1,1,1]
+}
+]
+
+module.exports = friends;
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,19 @@
+var path = require("path");
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+
+  
+};
